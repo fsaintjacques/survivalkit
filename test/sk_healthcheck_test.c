0279caef3c60fa91d00bb21e4b4abed85de192a9
François Saint-Jacques <fsaintjacques@gmail.com>
2017-04-30 11:50:26 -0400

    Simplify healthcheck test

--- test/sk_healthcheck_test.c
+++ test/sk_healthcheck_test.c
@@ -14,7 +14,7 @@ bool_healthcheck(const void *opaque, sk_error_t *err)
 	if (ck_pr_load_int(state)) {
 		return SK_HEALTH_OK;
 	} else {
-		sk_error_msg_code(err, "bool is in a critical state", 1);
+		sk_error_msg_code(err, "int is in a critical state", 1);
 		return SK_HEALTH_CRITICAL;
 	}
 }
@@ -22,15 +22,13 @@ bool_healthcheck(const void *opaque, sk_error_t *err)
 void
 healthcheck_test_basic()
 {
+	enum sk_health health = SK_HEALTH_UNKNOWN;
 	sk_healthcheck_t hc;
-	sk_error_t err;
+	sk_error_t error;
 	int state = 1;
 
 	assert_true(sk_healthcheck_init(
-	    &hc, "basic", "", 0, bool_healthcheck, &state, &err));
-
-	enum sk_health health = SK_HEALTH_UNKNOWN;
-	sk_error_t error;
+	    &hc, "basic", "", 0, bool_healthcheck, &state, &error));
 
 	assert_true(sk_healthcheck_poll(&hc, &health, &error));
 	assert_int_equal(health, SK_HEALTH_OK);
