378100590ec8fda3874d02a4327639f47b586be0
François Saint-Jacques <fsaintjacques@gmail.com>
2017-04-30 21:43:07 -0400

    Reformat sk_cc.h

--- include/sk_cc.h
+++ include/sk_cc.h
@@ -4,23 +4,23 @@
 #define SK_CACHE_SIZE 64
 #endif
 
-
 /* Function attributes */
-#define sk_printf(a, b) __attribute__((format (printf, a, b)))
-#define sk_nonnull(...) __attribute__((nonnull ( __VA_ARGS__ )))
+#define sk_printf(a, b) __attribute__((format(printf, a, b)))
+#define sk_nonnull(...) __attribute__((nonnull(__VA_ARGS__)))
 #define sk_constructor __attribute__((constructor))
 #define sk_destructor __attribute__((destructor))
 
 /* Type attributes */
-#define sk_align(n) __attribute__((aligned (n)))
+#define sk_align(n) __attribute__((aligned(n)))
 #define sk_packed __attribute__((packed))
 #define sk_cache_aligned sk_align(SK_CACHE_SIZE)
 
 /* sk_log requires 2 attributes */
-#define sk_log_attr __attribute__((format (printf, 4, 5), nonnull(1, 4)))
+#define sk_log_attr __attribute__((format(printf, 4, 5), nonnull(1, 4)))
 
-#define sk_likely(x)       __builtin_expect((x),1)
-#define sk_unlikely(x)     __builtin_expect((x),0)
+#define sk_likely(x) __builtin_expect((x), 1)
+#define sk_unlikely(x) __builtin_expect((x), 0)
+#define sk_array_size(x) (sizeof(x) / sizeof(x[0]))
 
 /* Structure that encapsulate file, function and line reference */
 struct sk_debug {
@@ -30,4 +30,8 @@ struct sk_debug {
 };
 typedef struct sk_debug sk_debug_t;
 
-#define sk_debug (sk_debug_t){__FILE__, __func__, __LINE__}
+#define sk_debug                                                               \
+	(sk_debug_t)                                                               \
+	{                                                                          \
+		__FILE__, __func__, __LINE__                                           \
+	}
